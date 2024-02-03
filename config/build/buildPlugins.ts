7244f19ba2e58d79ef9c3a48118fb3a37ea194ac
Eurodancer26 <murushkin.timur@gmail.com>
2024-02-03 21:56:51 +0300

    add

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,30 +1,24 @@
+import { BuildOptions } from './types/config';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
+import webpack, { DefinePlugin, HotModuleReplacementPlugin } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    const plugins = [
+    return [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
+            filename: 'css/[name].[contenthash:5].css',
         }),
-        new webpack.DefinePlugin({
+        new DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-    ];
-
-    if (isDev) {
-        plugins.push(new webpack.HotModuleReplacementPlugin());
-        plugins.push(new BundleAnalyzerPlugin({
+        new HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
             openAnalyzer: false,
-        }));
-    }
-
-    return plugins;
+        })
+    ];
 }
