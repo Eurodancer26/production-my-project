289c8c2167f8fb537dff050f7f438655413a59ed
Eurodancer26 <murushkin.timur@gmail.com>
2024-02-03 21:52:58 +0300

    add checks

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,24 +1,30 @@
-import { BuildOptions } from './types/config';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack, { DefinePlugin, HotModuleReplacementPlugin } from 'webpack';
+import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:5].css',
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-        new DefinePlugin({
+        new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        })
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+
+    return plugins;
 }
