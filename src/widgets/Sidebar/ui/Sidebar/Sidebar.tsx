5b2f26fc424cb10f0358b06277306abdedbf7480
Eurodancer26 <murushkin.timur@gmail.com>
2024-02-04 10:33:18 +0300

    fix dev sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,10 +1,10 @@
-import cls from './Sidebar.module.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
-
 import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import LangSwitcher from 'widgets/LangSwitcher/LangSwitcher';
 import {Button} from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
     className?: string;
@@ -12,7 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-
+    const {t} = useTranslation();
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
@@ -28,7 +28,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 type="button"
                 onClick={onToggle}
             >
-                TOGGLE
+                {t('TOGGLE')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
