8780e4d16ab3af34084b65625ff2db0a746e5a48
Eurodancer26 <murushkin.timur@gmail.com>
2024-02-04 19:02:57 +0300

    fix name ThemeDecorator

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -2,7 +2,7 @@ import { StoryFn } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 
 export function ThemeDecorator(theme: Theme) {
-    return function (StoryComponent: StoryFn) {
+    return function component(StoryComponent: StoryFn) {
         return (
             <div className={`app ${theme}`}>
                 <StoryComponent />
